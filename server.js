d55287e59c704260de23880f2e14d9af297a4194
Mishnah <dagmawyasfaw@gmail.com>
2024-10-27 13:01:43 +0300

    Updated server.js and database.js to use PostgreSQL

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg'); // Import the pg library
